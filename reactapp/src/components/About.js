fecdd237db06303cee67846bb0207bab5ee9b3b3
Robert McCurdy <rwmccurdy@hotmail.com>
2021-11-11 16:45:34 -0700

    About Page Comment

--- reactapp/src/components/About.js
+++ reactapp/src/components/About.js
@@ -2,6 +2,7 @@ import React from 'react';
 
 const About = () => {
     return (
+        // Include information about the software and how MyGH works
         <>
             <div style={{ marginTop: '64px' }}>About Page</div>
         </>
